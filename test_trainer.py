2b67d0fe687f35f1093631042ee2170c2fe2735e
jeorgejoshi <85611495+wp225@users.noreply.github.com>
2024-06-19 15:36:22 +0545

    Model Trainer

--- test_trainer.py
+++ test_trainer.py
@@ -1,4 +1,4 @@
-import mlflow
+import mlruns
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_diabetes
@@ -8,14 +8,14 @@ import dagshub
 dagshub.init(repo_owner='georgejoshi10', repo_name='MLflow', mlflow=True)
 
 
-mlflow.set_tracking_uri('https://dagshub.com/georgejoshi10/MLflow.mlflow')
+mlruns.set_tracking_uri('https://dagshub.com/georgejoshi10/MLflow.mlflow')
 # mlflow.set_tracking_uri('http://ec2-54-237-116-22.compute-1.amazonaws.com:5000')
 
 
 db = load_diabetes()
 X_train, X_test, y_train, y_test = train_test_split(db.data, db.target)
 
-with mlflow.start_run(run_name='test'):
+with mlruns.start_run(run_name='test'):
     rf = RandomForestRegressor(n_estimators=100, max_depth=6, max_features=3)
     rf.fit(X_train, y_train)
 
@@ -24,5 +24,5 @@ with mlflow.start_run(run_name='test'):
     mse = mean_squared_error(y_test, preds)
     r2 = r2_score(y_test, preds)
 
-    mlflow.log_metric('mean_squared_error', mse)
-    mlflow.log_metric('r2_score', r2)
+    mlruns.log_metric('mean_squared_error', mse)
+    mlruns.log_metric('r2_score', r2)
